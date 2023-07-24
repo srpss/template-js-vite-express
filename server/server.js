fab0548de62658d87ffc5fdb93067bde89285a9d
srpss <61586478+srpss@users.noreply.github.com>
2023-07-24 16:34:03 +0200

    remove testing code

--- server/server.js
+++ server/server.js
@@ -75,23 +75,6 @@ async function main() {
   await mongoose.connect(`mongodb://127.0.0.1:27017/${SITE_DB_NAME}`);
 }
 
-const testSchema = new mongoose.Schema({
-  name: String
-});
-
-const Test = mongoose.model('Test', testSchema);
-
-async function createTest(){
-  const silence = new Test({ name: 'Silence' });
-  await silence.save();
-  console.log(silence.name); // 'Silence'
-}
-
-async function find(){
-  const result = await Test.find({ name: /^Silence/ });
-  console.log(result)
-}
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
